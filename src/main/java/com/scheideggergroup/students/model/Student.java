b81c532630a23632ba5a6e532b4a3aa4377d9c4f
Vinicius Scheidegger <vinicius.scheidegger@gft.com>
2019-12-17 02:42:59 +0100

    Remove unused import

--- src/main/java/com/scheideggergroup/students/model/Student.java
+++ src/main/java/com/scheideggergroup/students/model/Student.java
@@ -3,7 +3,6 @@ package com.scheideggergroup.students.model;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
